643e6b90d6a4d90c027da9fbd0808a25bfab348c
Sebastian Schmidl <sebastian.schmidl@sap.com>
2015-11-01 23:23:15 +0100

    rest of files

---
+++ AndroidApp/Unveiled/app/src/main/java/sas_systems/unveiled/MainActivity.java
@@ -0,0 +1,13 @@
+package sas_systems.unveiled;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
