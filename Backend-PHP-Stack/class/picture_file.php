c63151417523af7246365a414f4b97cf274f5218
Sebastian Adams <sebastian.adams@me.com>
2015-11-08 17:14:38 +0100

    implemented: PictureFile->newFromId

--- Backend-PHP-Stack/class/picture_file.php
+++ Backend-PHP-Stack/class/picture_file.php
@@ -35,6 +35,57 @@ class PictureFile extends File
         $this->width = $width;
     }
 
+
+    /**
+     * creates a new object of this class using id
+     * @param int $id
+     */
+    public function newFromId($id)
+    {
+        global $dbConn;
+
+        if (is_int($id)) {
+
+            $res = $dbConn->query("SELECT * FROM file WHERE id = '$id'");
+            $row = $res->fetch_object();
+
+            //Logg all MySQL errors
+            if ($dbConn->error != "") {
+
+                //Log error
+                errorLog::newEntry("MySQL error: " . $dbConn->error, 2, __FILE__, __CLASS__, __FUNCTION__);
+            }
+
+            if ($row != null) {
+
+                $db_id = (int)$row->id;
+                $db_ownerId = (int)$row->owner_id;
+                $db_caption = utf8_encode($row->caption);
+                $db_filename = utf8_encode($row->filename);
+                $db_mediatype = utf8_encode($row->mediatype);
+                $db_uploadedAt = (int)$row->uploaded_at;
+                $db_size = (int)$row->size;
+                $db_lat = (float)$row->lat;
+                $db_lng = (float)$row->lng;
+                $db_public = intToBool((int)$row->public);
+                $db_verified = intToBool((int)$row->verified);
+                $db_height = (int)$row->height;
+                $db_width = (int)$row->width;
+
+                return new VideoFile($db_id, $db_ownerId, $db_caption, $db_filename, $db_mediatype, $db_uploadedAt, $db_size, $db_lat, $db_lng, $db_public, $db_verified, $db_height, $db_width);
+
+            } else {
+
+                return null;
+            }
+
+        } else {
+
+            return null;
+        }
+    }
+
+
     /**
      * @return int
      */
