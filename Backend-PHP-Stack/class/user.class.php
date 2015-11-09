9f34f272879cf73b2f41604deef5ca8180cfcadf
Sebastian Adams <sebastian.adams@me.com>
2015-11-09 09:50:54 +0100

    change the flushDB method and added UserPermission class

--- Backend-PHP-Stack/class/user.class.php
+++ Backend-PHP-Stack/class/user.class.php
@@ -3,35 +3,41 @@
 class User
 {
 
-    private $id;
-    private $username;
-    private $email;
-    private $emailNotification; // Int
-    private $mobileNumber;
-    private $mobileNumberNotification; // Int
-    private $password; //Crypt
-    private $token;
-    private $lastIP;
-    private $lastLogin; //Timestamp
-    private $permission;
+    private $id = -1;
+    private $username = "";
+    private $email = "";
+    private $emailNotification = false; // Int
+    private $password = ""; //Crypt
+    private $token = "";
+    private $lastIP = "";
+    private $lastLogin = 0; //Timestamp
+    private $permission = new UserPermission(0);
 
-
-    private function __construct($id, $email, $emailNotification, $mobileNumber, $mobileNumberNotification, $username, $password, $token, $lastIP, $lastLogin, $permission)
+    /**
+     * User constructor.
+     * @param int $id
+     * @param string $username
+     * @param string $email
+     * @param bool $emailNotification
+     * @param string $password
+     * @param string $token
+     * @param string $lastIP
+     * @param int $lastLogin
+     * @param UserPermission $permission
+     */
+    public function __construct($id, $username, $email, $emailNotification, $password, $token, $lastIP, $lastLogin, UserPermission $permission)
     {
-
-        $this->id = (int)$id;
+        $this->id = $id;
         $this->username = $username;
         $this->email = $email;
         $this->emailNotification = $emailNotification;
-        $this->mobileNumber = $mobileNumber;
-        $this->mobileNumberNotification = $mobileNumberNotification;
         $this->password = $password;
         $this->token = $token;
         $this->lastIP = $lastIP;
         $this->lastLogin = $lastLogin;
         $this->permission = $permission;
-
     }
+
 
     public static function NewFromToken($id, $token)
     {
@@ -57,17 +63,14 @@ class User
                 $db_id = (int)$row->id;
                 $db_username = utf8_encode($row->username);
                 $db_email = $row->email;
-                $db_emailNotification = (int)$row->email_notification_flag;
-                $db_mobileNumber = $row->mobile_number;
-                $db_mobileNumberNotification = (int)$row->mobile_number_notification_flag;
+                $db_emailNotification = intToBool((int)$row->email_notification_flag);
                 $db_password = $row->password;
                 $db_token = $row->token;
                 $db_lastIP = $row->last_ip;
                 $db_lastLogin = (int)$row->last_login;
-                $db_permission = (int)$row->permission;
+                $db_permission = new UserPermission((int)$row->permission);
 
-                $u = new User($db_id, $db_email, $db_emailNotification, $db_mobileNumber, $db_mobileNumberNotification, $db_username, $db_password, $db_token, $db_lastIP, $db_lastLogin, $db_permission);
-                return $u;
+                return new User($db_id, $db_username, $db_email, $db_emailNotification, $db_password, $db_token, $db_lastIP, $db_lastLogin, $db_permission);
             } else {
 
                 return null;
@@ -114,18 +117,14 @@ class User
                 $db_id = (int)$row->id;
                 $db_username = utf8_encode($row->username);
                 $db_email = $row->email;
-                $db_emailNotification = (int)$row->email_notification_flag;
-                $db_mobileNumber = $row->mobile_number;
-                $db_mobileNumberNotification = (int)$row->mobile_number_notification_flag;
+                $db_emailNotification = intToBool((int)$row->email_notification_flag);
                 $db_password = $row->password;
                 $db_token = $row->token;
                 $db_lastIP = $row->last_ip;
                 $db_lastLogin = (int)$row->last_login;
-                $db_permission = (int)$row->permission;
+                $db_permission = new UserPermission((int)$row->permission);
 
-                $u = new User($db_id, $db_email, $db_emailNotification, $db_mobileNumber, $db_mobileNumberNotification, $db_username, $db_password, $db_token, $db_lastIP, $db_lastLogin, $db_permission);
-                return $u;
-
+                return new User($db_id, $db_username, $db_email, $db_emailNotification, $db_password, $db_token, $db_lastIP, $db_lastLogin, $db_permission);
             } else {
 
                 return null;
@@ -169,19 +168,16 @@ class User
             $db_id = (int)$row->id;
             $db_username = utf8_encode($row->username);
             $db_email = $row->email;
-            $db_emailNotification = (int)$row->email_notification_flag;
-            $db_mobileNumber = $row->mobile_number;
-            $db_mobileNumberNotification = (int)$row->mobile_number_notification_flag;
+            $db_emailNotification = intToBool((int)$row->email_notification_flag);
             $db_password = $row->password;
             $db_token = $row->token;
             $db_lastIP = $row->last_ip;
             $db_lastLogin = (int)$row->last_login;
-            $db_permission = (int)$row->permission;
+            $db_permission = new UserPermission((int)$row->permission);
 
             if (crypt($password, $gvCryptSalt) == $db_password) {
 
-                $u = new User($db_id, $db_email, $db_emailNotification, $db_mobileNumber, $db_mobileNumberNotification, $db_username, $db_password,
-                    $db_token, $db_lastIP, $db_lastLogin, $db_permission);
+                $u = new User($db_id, $db_username, $db_email, $db_emailNotification, $db_password, $db_token, $db_lastIP, $db_lastLogin, $db_permission);
 
                 $u->setToken(self::generateToken($db_id));
                 $u->setLastLogin(time());
@@ -203,42 +199,137 @@ class User
 
     }
 
-
+    /**
+     * @return int
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
     public function getUsername()
     {
         return $this->username;
     }
 
+    /**
+     * @param string $username
+     */
     public function setUsername($username)
     {
         $this->username = $username;
     }
 
+    /**
+     * @return string
+     */
     public function getEmail()
     {
         return $this->email;
     }
 
+    /**
+     * @param string $email
+     */
     public function setEmail($email)
     {
         $this->email = $email;
     }
 
-    public function getMobileNumber()
+    /**
+     * @return boolean
+     */
+    public function isEmailNotification()
     {
-        return $this->mobileNumber;
+        return $this->emailNotification;
     }
 
-    public function setMobileNumber($mobileNumber)
+    /**
+     * @param boolean $emailNotification
+     */
+    public function setEmailNotification($emailNotification)
     {
-        $this->mobileNumber = (int)$mobileNumber;
+        $this->emailNotification = $emailNotification;
     }
 
+    /**
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param string $token
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastIP()
+    {
+        return $this->lastIP;
+    }
+
+    /**
+     * @param string $lastIP
+     */
+    public function setLastIP($lastIP)
+    {
+        $this->lastIP = $lastIP;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLastLogin()
+    {
+        return $this->lastLogin;
+    }
+
+    /**
+     * @param int $lastLogin
+     */
+    public function setLastLogin($lastLogin)
+    {
+        $this->lastLogin = $lastLogin;
+    }
+
+    /**
+     * @return UserPermission
+     */
+    public function getPermission()
+    {
+        return $this->permission;
+    }
+
+    /**
+     * @param UserPermission $permission
+     */
+    public function setPermission($permission)
+    {
+        $this->permission = $permission;
+    }
+
+    /**
+     * @return string
+     */
     public function getPassword()
     {
         return $this->password;
@@ -261,103 +352,79 @@ class User
         return false;
     }
 
-    public function getToken()
-    {
-        return $this->token;
-    }
+    public function existsInDB() {
 
-    public function setToken($token)
-    {
-        $this->token = $token;
-    }
+        if($this->id > 0) {
 
-    public function getLastIP()
-    {
-        return $this->lastIP;
-    }
+            return true;
+        }
+        else {
 
-    public function setLastIP($lastIP)
-    {
-        $this->lastIP = $lastIP;
-    }
-
-    public function getLastLogin()
-    {
-        return $this->lastLogin;
-    }
-
-    public function setLastLogin($lastLogin)
-    {
-
-        $this->lastLogin = (int)$lastLogin;
-    }
-
-    public function getEmailNotification()
-    {
-        return $this->emailNotification;
-    }
-
-    public function setEmailNotification($emailNotification)
-    {
-        $this->emailNotification = (int)$emailNotification;
-    }
-
-    public function getPermission()
-    {
-        return $this->permission;
-    }
-
-    public function setPermission($permission)
-    {
-        $this->permission = (int)$permission;
-    }
-
-    public function getMobileNumberNotification()
-    {
-        return $this->mobileNumberNotification;
-    }
-
-    public function setMobileNumberNotification($mobileNumberNotification)
-    {
-        $this->mobileNumberNotification = (int)$mobileNumberNotification;
+            return false;
+        }
     }
 
 
     public function flushDB()
     {
         global $dbConn;
-
-        $query = "UPDATE `user` SET `username`=?, `email`=?, `email_notification_flag`=?, `mobile_number`=?, `mobile_number_notification_flag`=?, `password`=?, `token`=?, `last_ip`=?, `last_login`=?, `permission`=? WHERE `id`=? ";
-        $query_stmt = $dbConn->prepare($query);
+        global $gvCryptSalt;
 
         $id = (int)$this->id;
         $username = utf8_decode(strip_tags($this->username));
         $email = $this->email;
-        $emailNotification = (int)$this->emailNotification;
-        $mobileNumber = (int)$this->mobileNumber;
-        $mobileNumberNotification = (int)$this->mobileNumberNotification;
+        $emailNotification = boolToInt($this->emailNotification);
         $password = $this->password;
         $token = $this->token;
         $lastIP = $this->lastIP;
         $lastLogin = (int)$this->lastLogin;
         $permission = (int)$this->permission;
 
-        $query_stmt->bind_param('ssiiisssiii', $username, $email, $emailNotification, $mobileNumber, $mobileNumberNotification, $password, $token, $lastIP, $lastLogin, $permission, $id);
-        $query_stmt->execute();
+        //object exists in DB
+        if($this->existsInDB()) {
 
-        //Logg all MySQL errors
-        if ($dbConn->error != "") {
+            $query = "UPDATE `user` SET `username`=?, `email`=?, `email_notification_flag`=?, `password`=?, `token`=?, `last_ip`=?, `last_login`=?, `permission`=? WHERE `id`=? ";
+            $query_stmt = $dbConn->prepare($query);
 
-            //Log error
-            errorLog::newEntry("MySQL error: " . $dbConn->error, 2, __FILE__, __CLASS__, __FUNCTION__);
+            $query_stmt->bind_param('ssisssiii', $username, $email, $emailNotification, $password, $token, $lastIP, $lastLogin, $permission, $id);
+            $query_stmt->execute();
 
-            return false;
+            //Logg all MySQL errors
+            if ($dbConn->error != "") {
+
+                //Log error
+                errorLog::newEntry("MySQL error: " . $dbConn->error, 2, __FILE__, __CLASS__, __FUNCTION__);
+
+                return false;
+            }
+            else {
+
+                return true;
+            }
+        }
+        else {
+
+            $query = "INSERT INTO user (username, email, email_notification_flag, password, token, last_ip, last_login, permission) VALUES (?,?,?,?,?,?,?,?)";
+            $query_stmt = $dbConn->prepare($query);
+
+            $query_stmt->bind_param('ssisssii', $username, $email, $emailNotification, $password, $token, $lastIP, $lastLogin, $permission);
+            $query_stmt->execute();
+
+            if ($dbConn->affected_rows > 0) {
+
+                $this->setId((int)$dbConn->insert_id);
+
+                return true;
+            }
+            else {
+
+                return false;
+            }
         }
 
-        return true;
+
     }
-
-
+    
     public function setSession()
     {
 
@@ -371,7 +438,7 @@ class User
     public static function unsetSession()
     {
 
-        setcookie("loginToken", "", time() - 100, "/");
+        setcookie("loginID", "", time() - 100, "/");
         setcookie("loginToken", "", time() - 100, "/");
     }
 
@@ -402,90 +469,11 @@ class User
         return $token;
     }
 
-
-    public static function create($username, $email, $emailNotification, $mobileNumber, $mobileNumberNotification, $permission, $password)
-    {
-        global $dbConn;
-        global $gvCryptSalt;
-
-        $username = utf8_decode(strip_tags($username));
-        $password = crypt($dbConn->real_escape_string($password), $gvCryptSalt);
-
-        $query = "INSERT INTO user (username, email, email_notification_flag, mobile_number, mobile_number_notification_flag,
-permission, password, last_login, last_ip) VALUES (?,?,?,?,?,?,?,?,?)";
-        $query_stmt = $dbConn->prepare($query);
-
-        $ip = $_SERVER['REMOTE_ADDR'];
-        $now = time();
-
-        $query_stmt->bind_param('ssiiiisis', $username, $email, $emailNotification, $mobileNumber,
-            $mobileNumberNotification, $permission, $password, $now, $ip);
-        $query_stmt->execute();
-
-        if ($dbConn->affected_rows > 0) {
-
-            return self::NewFromId($dbConn->insert_id);
-        } else {
-
-            return null;
-        }
-
-    }
-
-
     public static function generatePassword($pwd)
     {
         global $gvCryptSalt;
 
         return crypt($pwd, $gvCryptSalt);
-    }
-
-    /**
-     * Check user permission
-     * 0 = Read
-     * 1 = Read & Write
-     * 2 = Mod
-     * 3 = Owner
-     * 4 = Admin
-     * @param $permission
-     * @return bool
-     */
-    public function hasPermission($permission)
-    {
-
-        if ($this->permission >= $permission) {
-
-            return true;
-        }
-
-        return false;
-    }
-
-    public static function convertPerssionCode($code)
-    {
-
-        switch ($code) {
-
-            case 0:
-                return "Read";
-                break;
-
-            case 1:
-                return "Read & Write";
-                break;
-
-            case 2:
-                return "Moderator";
-                break;
-
-            case 3:
-                return "Owner";
-                break;
-
-            case 4:
-                return "Admin";
-                break;
-        }
     }
 
 
@@ -513,8 +501,6 @@ permission, password, last_login, last_ip) VALUES (?,?,?,?,?,?,?,?,?)";
             $db_username = utf8_encode($row->username);
             $db_email = $row->email;
             $db_emailNotification = (int)$row->email_notification_flag;
-            $db_mobileNumber = $row->mobile_number;
-            $db_mobileNumberNotification = (int)$row->mobile_number_notification_flag;
             $db_lastIP = $row->last_ip;
 
 
@@ -530,8 +516,7 @@ permission, password, last_login, last_ip) VALUES (?,?,?,?,?,?,?,?,?)";
             }
 
             $tmp[] = array("id" => $db_id, "username" => $db_username, "email" => $db_email, "emailNotification" => $db_emailNotification,
-                "mobileNumber" => $db_mobileNumber, "mobileNumberNotification" => $db_mobileNumberNotification, "lastIP" => $db_lastIP,
-                "lastLogin" => $db_lastLogin, "permission" => $db_permission);
+                "lastIP" => $db_lastIP, "lastLogin" => $db_lastLogin, "permission" => $db_permission);
         }
 
         return $tmp;
