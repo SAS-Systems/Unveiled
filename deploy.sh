d75fa94449383c331219cd92a9259239ac4c2085
CodeLionX <sebastian.schmidl@t-online.de>
2015-11-12 18:52:40 +0100

    next test with debugging output

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 # upload each file via ftp
 for file in Backend-PHP-Stack/*
 do
+	echo "upload $file to $FTP_URL/test/";
 	curl --ftp-create-dirs -T "$file" -u $FTP_USER:$FTP_PASSWORD "$FTP_URL/test/";
 done; # file
