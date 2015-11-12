ab318893da096e34ec4a17421a34d2f01ff77316
Sebastian Adams <sebastian.adams@me.com>
2015-11-12 15:01:17 +0100

    added camera and file-upload activity

--- UnveiledApp/app/src/main/java/de/systemgrid/sas/unveiledapp/MainActivity.java
+++ UnveiledApp/app/src/main/java/de/systemgrid/sas/unveiledapp/MainActivity.java
@@ -1,5 +1,6 @@
 package de.systemgrid.sas.unveiledapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bt_camera;
+    private Button bt_upload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        bt_camera = (Button) findViewById(R.id.bt_camera);
+        bt_upload = (Button) findViewById(R.id.bt_upload);
+
+        setListener();
 
     }
 
@@ -40,5 +50,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setListener() {
+
+        bt_camera.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent camera = new Intent(MainActivity.this, CameraActivity.class);
+                startActivity(camera);
+            }
+        });
+
+        bt_upload.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent uploadFiles = new Intent(MainActivity.this, UploadFileActivity.class);
+                startActivity(uploadFiles);
+            }
+        });
     }
 }
