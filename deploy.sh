9733895f92317cb36d2487999ff8d6f733ae92c3
CodeLionX <sebastian.schmidl@t-online.de>
2015-11-12 20:34:22 +0100

    more bugfixes

--- deploy.sh
+++ deploy.sh
@@ -9,19 +9,19 @@ cd Backend-PHP-Stack
 #   --> extend if necessary <--
 case "$BRANCH" in
 	'Backend-PHP-Stack')
-		FOLDER = 'php'
+		FOLDER = "php"
 		;;
 #	'someBranch' | 'otherBranch')
-#		FOLDER = 'common/folder/for/both/branchs'
+#		FOLDER = "common/folder/for/both/branchs"
 #		;;
 	*)
-		FOLDER = 'test/php'
+		FOLDER = "test/php"
 		;;
 esac
 
 # iterate over files and upload them with curl
 for FILE in $(find . -type f)
 do
-	printf "\nupload $file to server at php/test/$file";
-	curl --ftp-create-dirs -T "$file" -u $FTP_USER:$FTP_PASSWORD "$FTP_URL/$FOLDER/$FILE";
+	printf "\nupload $FILE to server at $FOLDER/$FILE";
+	curl --ftp-create-dirs -T "$FILE" -u $FTP_USER:$FTP_PASSWORD "$FTP_URL/$FOLDER/$FILE";
 done; # file
