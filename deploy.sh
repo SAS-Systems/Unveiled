9f37cb5f21da5b3980266898712a1a3796cd0acf
CodeLionX <sebastian.schmidl@t-online.de>
2015-11-12 21:08:40 +0100

    dry run

--- deploy.sh
+++ deploy.sh
@@ -4,24 +4,24 @@
 # upload each php file via ftp
 #########################################
 
-cd Backend-PHP-Stack
 # branch-dependent folder:
 #   --> extend if necessary <--
 case "$BRANCH" in
 	'Backend-PHP-Stack')
-		PATH = "php"
+		PFAD = "php"
 		;;
 #	'someBranch' | 'otherBranch')
-#		PATH = "common/folder/for/both/branches"
+#		PFAD = "common/folder/for/both/branches"
 #		;;
 	*)
-		PATH = "test/php"
+		PFAD = "test/php"
 		;;
 esac
 
 # iterate over files and upload them with curl
+cd Backend-PHP-Stack
 for FILE in $(find . -type f)
 do
-	printf "\nupload $FILE to server at $PATH/$FILE";
-	curl --ftp-create-dirs -T "$FILE" -u $FTP_USER:$FTP_PASSWORD "$FTP_URL/$PATH/$FILE";
+	printf "\nupload $FILE to server at $PFAD/$FILE";
+#	curl --ftp-create-dirs -T "$FILE" -u $FTP_USER:$FTP_PASSWORD "$FTP_URL/$PFAD/$FILE";
 done; # file
