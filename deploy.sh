a08bffb6fcb86c188f42b36929db090c34665ca4
CodeLionX <sebastian.schmidl@t-online.de>
2015-11-12 18:45:00 +0100

    ...

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 # upload each file via ftp
 for file in Backend-PHP-Stack/*
 do
-	curl --ftp-create-dirs -T $file -u $FTP_USER:$FTP_PASSWORD $FTP_URL/test/;
+	curl --ftp-create-dirs -T "$file" -u $FTP_USER:$FTP_PASSWORD "$FTP_URL/test/";
 done; # file
