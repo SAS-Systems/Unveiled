89d7c260a5e0856a550fb65b2605b2cac2264779
CodeLionX <sebastian.schmidl@t-online.de>
2015-11-12 20:23:33 +0100

    test script again in after_success hook

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-# upload each file via ftp
+#########################################
+# upload each php file via ftp
+#########################################
+
 cd Backend-PHP-Stack
-for file in $(find . -type f)
+# branch-dependent folder:
+#   --> extend if necessary <--
+case "$BRANCH" in
+	'Backend-PHP-Stack')
+		FOLDER = 'php'
+#	'someBranch' | 'otherBranch')
+#		FOLDER = 'common/folder/for/both/branchs'
+	*)
+		FOLDER = 'test/php'
+esac
+
+# iterate over files and upload them with curl
+for FILE in $(find . -type f)
 do
-	printf "\nupload $file to server at php/test/";
-	curl --ftp-create-dirs -T "$file" -u $FTP_USER:$FTP_PASSWORD "$FTP_URL/php/test/$file";
+	printf "\nupload $file to server at php/test/$file";
+	curl --ftp-create-dirs -T "$file" -u $FTP_USER:$FTP_PASSWORD "$FTP_URL/$FOLDER/$FILE";
 done; # file
