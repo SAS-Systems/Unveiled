b598936cead194bd779851b158457022012ed6f2
Sebastian Adams <sebastian.adams@me.com>
2015-11-15 12:32:27 +0100

    bug fixed

--- UnveiledApp/app/src/main/java/de/systemgrid/sas/unveiledapp/SettingsActivity.java
+++ UnveiledApp/app/src/main/java/de/systemgrid/sas/unveiledapp/SettingsActivity.java
@@ -262,7 +262,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
-            bindPreferenceSummaryToValue(findPreference("picture_local_save"));
+            //bindPreferenceSummaryToValue(findPreference("picture_local_save"));
         }
 
         @Override
