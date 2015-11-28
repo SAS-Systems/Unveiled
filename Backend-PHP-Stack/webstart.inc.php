1b8c88e3e73c44c272629d7320ceaa29050c963f
Sebastian Adams <sebastian.adams@me.com>
2015-11-28 17:12:47 +0100

    require bug fixed

--- Backend-PHP-Stack/webstart.inc.php
+++ Backend-PHP-Stack/webstart.inc.php
@@ -1,15 +1,13 @@
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-set_time_limit ( 999 );
-
-//testchange
+set_time_limit(999);
 
 ///////////////////////////////////////////////////////////////////////////
 //Config File
 
 //require_once( __DIR__.DIRECTORY_SEPARATOR.'config.inc.php' );
-require_once( '../config.inc.php' );
+require_once(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config.inc.php');
 
 ///////////////////////////////////////////////////////////////////////////
 
@@ -19,8 +17,8 @@ require_once( '../config.inc.php' );
 //MySQL Verbindung aufbauen
 $dbConn = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
 
-if($dbConn->connect_errno > 0){
-	die( 'MySQL Error: ' . $dbConn->connect_error . ']' );
+if ($dbConn->connect_errno > 0) {
+    die('MySQL Error: ' . $dbConn->connect_error . ']');
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -29,28 +27,28 @@ if($dbConn->connect_errno > 0){
 ///////////////////////////////////////////////////////////////////////////
 //Klasse laden
 
-require_once( __DIR__.DIRECTORY_SEPARATOR.'class'.DIRECTORY_SEPARATOR.'file.class.php' );
+require_once(__DIR__ . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'file.class.php');
 
-require_once( __DIR__.DIRECTORY_SEPARATOR.'class'.DIRECTORY_SEPARATOR.'video_file.class.php' );
+require_once(__DIR__ . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'video_file.class.php');
 
-require_once( __DIR__.DIRECTORY_SEPARATOR.'class'.DIRECTORY_SEPARATOR.'picture_file.class.php' );
+require_once(__DIR__ . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'picture_file.class.php');
 
-require_once( __DIR__.DIRECTORY_SEPARATOR.'class'.DIRECTORY_SEPARATOR.'message.class.php' );
+require_once(__DIR__ . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'message.class.php');
 
-require_once( __DIR__.DIRECTORY_SEPARATOR.'class'.DIRECTORY_SEPARATOR.'user.class.php' );
+require_once(__DIR__ . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'user.class.php');
 
-require_once( __DIR__.DIRECTORY_SEPARATOR.'class'.DIRECTORY_SEPARATOR.'error_log.class.php' );
+require_once(__DIR__ . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'error_log.class.php');
 
 ///////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////
 //Slim RESTful API
-require_once( __DIR__.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php' );
+require_once(__DIR__ . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
 ///////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////
 //Global Functions
 
-require_once( __DIR__.DIRECTORY_SEPARATOR.'class'.DIRECTORY_SEPARATOR.'globalfunctions.function.php' );
+require_once(__DIR__ . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'globalfunctions.function.php');
 
 ///////////////////////////////////////////////////////////////////////////
