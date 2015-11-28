8d81baf2af6413f8cae4cb78dc62b71520f95304
Sebastian Adams <sebastian.adams@me.com>
2015-11-28 14:53:57 +0100

    first REST API test

--- Backend-PHP-Stack/index.php
+++ Backend-PHP-Stack/index.php
@@ -6,4 +6,4 @@ require_once('webstart.inc.php');
 ///////////////////////////////////////////////////////////////////////////
 
 
-<?php print_r(get_loaded_extensions()); ?>
+print_r(get_loaded_extensions());
