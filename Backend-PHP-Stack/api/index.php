6b0c5489ba42e7f43c78b676e0f6438cd84ed48a
Sebastian Adams <sebastian.adams@me.com>
2015-11-28 17:01:59 +0100

    next try REST

--- Backend-PHP-Stack/api/index.php
+++ Backend-PHP-Stack/api/index.php
@@ -1,19 +1,18 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-set_time_limit ( 999 );
 
-
-require '../lib/vendor/autoload.php';
-
-echo "test";
+///////////////////////////////////////////////////////////////////////////
+//Webstart
+require_once('webstart.inc.php');
+///////////////////////////////////////////////////////////////////////////
 
 $app = new \Slim\Slim();
+
 
 $app->get('/hello/:name', function ($name) {
 
     echo "Hello, " . $name;
 });
+
 
 $app->get('/test', function () {
 
