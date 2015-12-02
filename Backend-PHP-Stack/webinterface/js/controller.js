e9d19f6e7f04fcfb2ef514e564b23c23b5bc8097
Digister <faschaefer@freenet.de>
2015-12-02 13:14:09 +0100

    Enabled Login and Logout , plus fixes

--- Backend-PHP-Stack/webinterface/js/controller.js
+++ Backend-PHP-Stack/webinterface/js/controller.js
@@ -18,6 +18,8 @@
         $log.log("was");
     }]);
 })();**/
+
+
 loadNavbar = function () {
     if (($.cookie("loginToken")) !== undefined) {
         $("#userMenu").css("visibility", "visible");
@@ -41,41 +43,44 @@ $(document).ready(function(){
             message: 'message'});
     });
 
-        $("#loginBtn").click(function(){
-            var username = $("#usrname").val();
-            var password = $("#psw").val();
-            var data = {"username":username,"password":password};
-            $.ajax({
-                url:"../api/user/login",
-                method: "POST",
-                data: data,
-                success: function(result){
-                    var res = JSON.parse(result);
-                    if(res.error ===0){
+    $("#loginBtn").click(function(){
+        var username = $("#usrname").val();
+        var password = $("#psw").val();
+        var data = {"username":username,"password":password};
+        $.ajax({
+            url:"../api/user/login",
+            method: "POST",
+            data: data,
+            success: function(result){
+                var res = JSON.parse(result);
+                if(res.error ===0){
                     loadNavbar();
                     $("#myModal").modal("toggle");
-                        $.toaster({ priority:'success',
-                            title:'Success',
-                            message: res.errorMsg});
-                    }
-                    else{
-                        $.toaster({ priority:'success',
-                            title: res.error,
-                            message: res.errorMsg});
-
-                    }
-                },
-                error: function(error){
                     $.toaster({ priority:'success',
-                        title:error.status,
-                        message: 'OOps seems like the server has some problems'});
+                        title:'Success',
+                        message: res.errorMsg});
                 }
-            });
+                else{
+                    $.toaster({ priority:'success',
+                        title: res.error,
+                        message: res.errorMsg});
+
+                }
+            },
+            error: function(error){
+                $.toaster({ priority:'success',
+                    title:error.status,
+                    message: 'OOps seems like the server has some problems'});
+            }
         });
+    });
 
     $("#logoutPage").click(function(){
         $("#userMenu").css("visibility","hidden");
         $("#loginPage").css("visibility","visible");
+        $.toaster({ priority:'success',
+            title:'Success',
+            message: 'Your now logged out'});
     });
 
 
