d4156f1c1a889b25ab332f37f48d5406782274c2
SeanAda <sebastian.adams@me.com>
2016-05-06 14:34:07 +0200

    implemented DAO interface

---
+++ Backend-PHP-Stack/class/dao.interface.php
@@ -0,0 +1,25 @@
+<?php
+
+interface DAOinterface {
+
+    /**
+     * return object from class
+     * @param $id
+     * @return object
+     */
+    public function newFromId($id);
+
+    /**
+     * flushed the object to DB
+     * @return boolean
+     */
+    public function flushDB();
+
+    /**
+     * check if the object exists in DB or if it's a new entry
+     * @return boolean
+     */
+    public function existsInDB();
+}
+
+?>
