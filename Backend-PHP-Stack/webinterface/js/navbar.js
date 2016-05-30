ada187b4296f797f877266fea3e398192c72dfd5
Digister <digister@protonmail.com>
2016-05-30 14:05:19 +0200

    Reworked Login and Popup

--- Backend-PHP-Stack/webinterface/js/navbar.js
+++ Backend-PHP-Stack/webinterface/js/navbar.js
@@ -86,21 +86,30 @@ $(document).ready(function(){
     });
 
     $("#deleteBtn").click(function(){
-        console.log(currentItem.currentItem.targetScope._currentItem.__id);
-        $.ajax({
-            url: "../api/file/"+currentItem.currentItem.targetScope._currentItem.__id,
-            type: "DELETE",
-            success: function(result){
-                var res = JSON.parse(result);
+        swal({   title: "Are you sure?",
+            text: "You will not be able to recover this imaginary file!",
+            type: "warning",   showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, delete it!",
+            closeOnConfirm: true },
+            function(){    $.ajax({
+                url: "../api/file/"+currentItem.currentItem.targetScope._currentItem.__id,
+                type: "DELETE",
+                success: function(result){
+                    var res = JSON.parse(result);
+                    swal("Deleted!", "Your file has been deleted.", "success");
+                    setTimeout(function(){ location.reload(); }, 2000);
 
-            },
-            error: function(error){
-                var res = {
-                    "error": error.status,
-                    "errorMsg":error.statusText,
-                    "errorType": "E"
-                };
-            }
+
+                },
+                error: function(error){
+                    var res = {
+                        "error": error.status,
+                        "errorMsg":error.statusText,
+                        "errorType": "E"
+                    };
+                }
+                });
 
 
         });
